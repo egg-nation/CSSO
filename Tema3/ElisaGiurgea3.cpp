bda493f566d899434f6c70405a940e0b48b33ab3
Elisa Giurgea <62005443+egg-nation@users.noreply.github.com>
2020-11-17 00:02:01 +0200

    Shit happens, doesn't it?

--- Tema3/ElisaGiurgea3.cpp
+++ Tema3/ElisaGiurgea3.cpp
@@ -1,246 +1,311 @@
-#include <windows.h>
 #include <iostream>
-#include <fstream>
+#include <Windows.h>
+#include <random>
+#include <cstdlib>
+#include <utility>
+#include <tuple>
+#include <memory>
 #include <optional>
 
 using namespace std;
 
 
-struct Data 
+struct Data
 {
     unsigned int value;
     unsigned int doubleValue;
 };
 
-
 template <typename Object, typename Deleter>
 struct WindowsPtr
 {
 private:
-	optional<Object> objectData;
-	Deleter deleter;
-	Object invalidObject;
+    optional<Object> objectData;
+    Deleter deleter;
+    Object invalidObject;
 
-	int* refCount = new int(0);
+    int* refCount = new int(0);
 
 public:
-	WindowsPtr() = default;
-	WindowsPtr(
-		const Object& obj,
-		Deleter deleter,
-		const Object& invalidObject) :
-		objectData(obj), deleter(deleter), invalidObject(invalidObject) {
-		*refCount = 1;
+    WindowsPtr() = default;
+    WindowsPtr(
+        const Object& obj,
+        Deleter deleter,
+        const Object& invalidObject) :
+        objectData(obj), deleter(deleter), invalidObject(invalidObject) {
+        *refCount = 1;
 
-	}
-	WindowsPtr(const WindowsPtr<Object, Deleter>& rhs) {
-		this->refCount = rhs.refCount;
-		(*refCount)++;
-		objectData = rhs.objectData;
-		deleter = rhs.deleter;
-		invalidObject = rhs.invalidObject;
-	};
+    }
+    WindowsPtr(const WindowsPtr<Object, Deleter>& rhs) {
+        this->refCount = rhs.refCount;
+        (*refCount)++;
+        objectData = rhs.objectData;
+        deleter = rhs.deleter;
+        invalidObject = rhs.invalidObject;
+    };
 
-	WindowsPtr(const WindowsPtr<Object, Deleter>&&) = delete;
+    WindowsPtr(const WindowsPtr<Object, Deleter>&&) = delete;
 
-	~WindowsPtr()
-	{
-		if (objectData.has_value() && *refCount == 1)
-		{
-			deleter(objectData.value());
-			objectData = invalidObject;
-			delete refCount;
-		}
-		*refCount -= 1;
-	}
+    ~WindowsPtr()
+    {
+        if (objectData.has_value() && *refCount == 1)
+        {
+            deleter(objectData.value());
+            objectData = invalidObject;
+            delete refCount;
+        }
+        *refCount -= 1;
+    }
 
-	WindowsPtr<Object, Deleter>& operator = (const Object& rhs) {
-		this->objectData = rhs;
-		return *this;
-	}
+    WindowsPtr<Object, Deleter>& operator = (const Object& rhs)
+    {
+        this->objectData = rhs;
+        return *this;
+    }
 
-	Object operator*() const {
-		return objectData.has_value() ? objectData.value() : invalidObject;
-	}
+    Object operator*() const
+    {
+        return objectData.has_value() ? objectData.value() : invalidObject;
+    }
 
 };
 
 #define HANDLE_PTR_OBJECT WindowsPtr<HANDLE, BOOL(__stdcall*)(HANDLE)>
 #define HANDLE_PTR_VIEW WindowsPtr<void*, BOOL(__stdcall*)(LPCVOID)>
 
+
 tuple<bool, HANDLE_PTR_OBJECT, HANDLE_PTR_VIEW>
-open_file_mapping_handle_and_view()
+create_file_mapping_handle_and_view()
 {
-	constexpr const unsigned int fileMappingSize = 1024 * 1024;
+    constexpr const unsigned int fileMappingSize = 1024 * 1024;
 
-	HANDLE fileMappingHandle = OpenFileMapping(
-		FILE_MAP_ALL_ACCESS,
-		FALSE,
-		"MultithreadedChecker");
+    HANDLE fileMappingHandle = CreateFileMapping(
+        INVALID_HANDLE_VALUE,
+        NULL, PAGE_READWRITE,
+        0,
+        fileMappingSize,
+        "MultithreadedChecker");
 
-	if (fileMappingHandle == NULL)
-	{
-		cout << "CreateFileMapping failed. Error code: " << GetLastError() << endl;
-		return {
-			false,
-			{},
-			{} };
-	}
+    if (fileMappingHandle == NULL)
+    {
+        cout << "CreateFileMapping failed. Error code: " << GetLastError() << endl;
+        return {
+            false,
+            {},
+            {} };
+    }
 
-	void* fileMapping = MapViewOfFile(
-		fileMappingHandle,
-		FILE_MAP_ALL_ACCESS,
-		0,
-		0,
-		0);
+    void* fileMapping = MapViewOfFile(
+        fileMappingHandle,
+        FILE_MAP_ALL_ACCESS,
+        0,
+        0,
+        0);
 
-	if (fileMapping == NULL)
-	{
-		CloseHandle(fileMappingHandle);
-		cout << "MapViewOfFile failed. Error code: " << GetLastError() << endl;
-		return {
-			false,
-			{},
-			{} };
-	}
+    if (fileMapping == NULL)
+    {
+        CloseHandle(fileMappingHandle);
+        cout << "MapViewOfFile failed. Error code: " << GetLastError() << endl;
+        return {
+            false,
+            {},
+            {} };
+    }
 
-	return {
-		true,
-		{fileMappingHandle, CloseHandle, INVALID_HANDLE_VALUE},
-		{fileMapping, UnmapViewOfFile, nullptr}
-	};
+    return {
+        true,
+        {fileMappingHandle, CloseHandle, INVALID_HANDLE_VALUE},
+        {fileMapping, UnmapViewOfFile, nullptr}
+    };
 }
 
- 
-int main(int argc, const char* argv[]) 
+int main(int argc, const char* argv[])
 {
-    if (argc != 2) 
-	{
+    unsigned int steps = 420;
+
+    if (argc != 3)
+    {
         cout << "Invalid number of arguments given" << endl;
         return 0;
     }
 
-    constexpr const unsigned int steps = 420;
+    auto fileMappingHandleView = create_file_mapping_handle_and_view();
 
+    if (!get<0>(fileMappingHandleView))
+    {
+        return 0;
+    }
 
-	auto fileMappingHandleView = open_file_mapping_handle_and_view();
+    auto fileMappingHandle = get<1>(fileMappingHandleView);
+    auto fileMappingView = get<2>(fileMappingHandleView);
 
-	if (!get<0>(fileMappingHandleView))
-	{
-		return 0;
-	}
+    random_device dev;
+    ranlux24 rng(dev());
+    uniform_int_distribution<unsigned int> randomValueGenerator(1, 1000);
 
-	auto fileMappingHandle = get<1>(fileMappingHandleView);
-	auto fileMappingView = get<2>(fileMappingHandleView);
+    if (string(argv[2]) == "mutex")
+    {
 
-	if (string(argv[1]) == "mutex") 
-	{
-		HANDLE mutexValue = OpenMutex(
-			SYNCHRONIZE,
-			FALSE,
-			"SynchronisedMutex");
+        HANDLE mutexValue;
+        mutexValue = CreateMutex(
+            NULL,
+            FALSE,
+            "SynchronisedMutex");
 
-		if (!mutexValue)
-		{
-			CloseHandle(mutexValue);
-			cout << "[Process 2] OpenMutex failed. Error code " << GetLastError() << endl;
-			return 0;
-		}
+        if (!mutexValue)
+        {
+            cout << "[Process 1] Create mutex failed. Last error = " << GetLastError() << endl;
+            return 0;
+        }
+        HANDLE_PTR_OBJECT mutex(
+            mutexValue,
+            CloseHandle,
+            INVALID_HANDLE_VALUE);
 
+        HANDLE_PTR_OBJECT processThread, processHandle;
 
-		HANDLE_PTR_OBJECT mutex(
-			mutexValue,
-			CloseHandle,
-			INVALID_HANDLE_VALUE);
+        PROCESS_INFORMATION newProcess;
+        STARTUPINFOA startupInfo = { 0 };
+        startupInfo.cb = sizeof(startupInfo);
 
-		int i = 0, index;
-		while (i < steps) 
-		{
-			WaitForSingleObject(*mutex, INFINITE);
+        string commandLine = argv[1];
+        commandLine.append(" mutex");
 
-			void* buffer = *fileMappingView;
+        BOOL processCreateResult = CreateProcessA(
+            NULL,
+            (LPSTR)commandLine.c_str(),
+            nullptr,
+            nullptr,
+            true,
+            0,
+            nullptr,
+            (LPSTR)"./",
+            &startupInfo,
+            &newProcess);
 
-			CopyMemory(
-				&index,
-				buffer, 
-				sizeof(int));
+        if (!processCreateResult)
+        {
+            cout << "Unable to create a new process. Last error = " << GetLastError() << endl;
+            return 0;
+        }
 
-			if (index >= i) 
-			{
-				Data values;
-				CopyMemory(
-					&values, 
-					(unsigned char*)buffer + sizeof(int) + index * sizeof(values), 
-					sizeof(values));
+        processThread = newProcess.hThread;
+        processHandle = newProcess.hProcess;
 
-				if (values.value * 2 != values.doubleValue) 
-				{
-					cout << "[Process 2] INCORRECT " << values.value << ' ' << values.doubleValue << endl;
-				} 
-				else 
-				{
-					cout << "[Process 2] CORRECT!\n";
-					cout.flush();
-				}
+        for (unsigned int i = 0; i < steps; ++i)
+        {
+            WaitForSingleObject(
+                *mutex,
+                INFINITE);
 
-				i += 1;
-			}
+            unsigned int randomValue = randomValueGenerator(rng);
 
-			ReleaseMutex(*mutex);
-		}
-	} 
-	else if (string(argv[1]) == "event") 
-	{
-		HANDLE_PTR_OBJECT writeEvent, readEvent;
-		{
-			HANDLE writeEventHandle = OpenEvent(
-				EVENT_MODIFY_STATE | SYNCHRONIZE,
-				0,
-				"WriteEvent");
+            Data sendData{ randomValue, 2 * randomValue };
 
-			if (writeEventHandle == NULL)
-			{
-				cout << "CreateEvent(WriteEvent) failed. Last error = " << GetLastError() << endl;
-				return 1;
-			}
+            void* viewAddress = *fileMappingView;
 
-			HANDLE readEventHandle = OpenEvent(
-				EVENT_MODIFY_STATE | SYNCHRONIZE,
-				0,
-				"ReadEvent");
+            CopyMemory(
+                viewAddress,
+                &i,
+                sizeof(unsigned int));
+            CopyMemory(
+                (unsigned char*)viewAddress + sizeof(int) + i * sizeof(Data),
+                &sendData,
+                sizeof(sendData));
 
-			if (readEventHandle == NULL)
-			{
-				cout << "CreateEvent(ReadEvent) failed. Last error = " << GetLastError() << endl;
-				return 1;
-			}
+            cout << "[Process 1] Written " << sendData.value << ' ' << sendData.doubleValue << endl;
 
-			writeEvent = writeEventHandle;
-			readEvent = readEventHandle;
-		}
+            ReleaseMutex(*mutex);
 
-		for (int i = 0; i < steps; ++i) 
-		{
-			WaitForSingleObject(*writeEvent, INFINITE);
-			Data values;
-			void* buffer = *fileMappingView;
-			CopyMemory(
-				&values, 
-				buffer, 
-				sizeof(Data));
+        }
 
-            if (values.value * 2 != values.doubleValue) 
-			{
-				cout << "[Process 2] INCORRECT " << values.value << ' ' << values.doubleValue << endl;
-			} 
-			else 
-			{
-				cout << "[Process 2] CORRECT!\n";
-				cout.flush();
-			}
+        WaitForSingleObject(
+            *processHandle,
+            INFINITE);
 
-			ResetEvent(*writeEvent);
-			SetEvent(*readEvent);
-		}
-	}
+    }
+    else if (string(argv[2]) == "event")
+    {
+        HANDLE_PTR_OBJECT writeEvent, readEvent, processThread, processHandle;
+        {
+            HANDLE writeEventHandle = CreateEvent(
+                NULL,
+                TRUE,
+                FALSE,
+                "WriteEvent");
+
+            if (writeEventHandle == NULL)
+            {
+                cout << "CreateEvent(WriteEvent) failed. Last error = " << GetLastError() << endl;
+                return 1;
+            }
+
+            HANDLE readEventHandle = CreateEvent(
+                NULL,
+                TRUE,
+                TRUE,
+                "ReadEvent");
+
+            if (readEventHandle == NULL)
+            {
+                cout << "CreateEvent(ReadEvent) failed. Last error = " << GetLastError() << endl;
+                return 1;
+            }
+
+            STARTUPINFOA startupInfo = { 0 };
+            PROCESS_INFORMATION newProcess;
+            startupInfo.cb = sizeof(startupInfo);
+
+            string commandLine = argv[1];
+            commandLine.append(" event");
+
+            BOOL processCreateResult = CreateProcessA(
+                NULL,
+                (LPSTR)commandLine.c_str(),
+                nullptr,
+                nullptr,
+                true,
+                0,
+                nullptr,
+                (LPSTR)"./",
+                &startupInfo,
+                &newProcess);
+
+            if (!processCreateResult)
+            {
+                cout << "Creating a new process failed. Last error = " << GetLastError() << endl;
+                return 0;
+            }
+
+            writeEvent = writeEventHandle;
+            readEvent = readEventHandle;
+            processThread = newProcess.hThread;
+            processHandle = newProcess.hProcess;
+        }
+
+        for (unsigned int i = 0; i < steps; ++i)
+        {
+            WaitForSingleObject(
+                *readEvent,
+                INFINITE);
+
+            unsigned int randomValue = randomValueGenerator(rng);
+            Data sendData{ randomValue, 2 * randomValue };
+            void* viewAddress = *fileMappingView;
+
+            CopyMemory(
+                viewAddress,
+                &sendData,
+                sizeof(sendData));
+
+            cout << "[Process 1] Written " << sendData.value << ' ' << sendData.doubleValue << endl;
+
+            ResetEvent(*readEvent);
+            SetEvent(*writeEvent);
+        }
+
+        WaitForSingleObject(
+            *processHandle,
+            INFINITE);
+    }
 }
